2b4ca48ed864fecf824fefcd700ad5eae4ea84b0
rimorgin <calvscalderonx@gmail.com>
2023-11-07 03:53:07 +0800

    fix navigation theme

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import * as SystemUI from 'expo-system-ui';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginRegis from './auth/LoginRegis';
 import GetStartedOTP from './auth/getStartedOTP';
 
 SystemUI.setBackgroundColorAsync('white');
 
+const NavTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'rgba(111, 202, 186, 1)',
+  },
+};
+
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <View style={styles.container}>
-      <NavigationContainer>
+      <NavigationContainer theme={NavTheme}>
         <Stack.Navigator initialRouteName="Login">
           <Stack.Screen name="Login" component={LoginRegis} options={ {headerShown: false} } />
           <Stack.Screen name="OTP Login/Register" component={GetStartedOTP}/>
